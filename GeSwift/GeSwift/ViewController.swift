66d8f433e38057713677e05ec70e5cbec6f3433d
my1325 <1173962595@qq.com>
2018-06-22 09:43:30 +0800

    init

---
+++ GeSwift/GeSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeSwift
+//
+//  Created by weipinzhiyuan on 2018/6/20.
+//  Copyright © 2018年 my. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
