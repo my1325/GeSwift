a542e163ffabe9974e048616876d7105673d1154
weipinzhiyuan <>
2019-06-10 16:42:14 +0800

    save

--- GeSwift/GeSwift/ViewController.swift
+++ GeSwift/GeSwift/ViewController.swift
@@ -15,20 +15,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-//        var testView = UILabel()
-//        testView.text = "xxxx"
-//        testView.backgroundColor = UIColor.red
-//        testView.ge.size = CGSize(width: 100, height: 100)
-//        testView.ge.x = 200
-//        testView.ge.y = 200
-//
-//        self.view.addSubview(testView)
-        scanView.rectOfInterest = CGRect(x: 100, y: 100, width: 200, height: 300)
-        scanView.scanResult = { [weak self] (string) in
-            print(string ?? "unknow")
-            self?.scanView.startScan()
-        }
-        scanView.startScan()
     }
 
     override func didReceiveMemoryWarning() {
